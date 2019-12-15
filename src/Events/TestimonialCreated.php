0b88d2a4f0fe6314667908ed0788a558e23e707f
Abdelrahman Omran <me@omranic.com>
2019-12-16 00:05:16 +0400

    Create event classes and map them in the model

---
+++ src/Events/TestimonialCreated.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rinvex\Testimonials\Events;
+
+use Illuminate\Queue\SerializesModels;
+use Rinvex\Testimonials\Models\Testimonial;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class TestimonialCreated implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $testimonial;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \Rinvex\Testimonials\Models\Testimonial $testimonial
+     */
+    public function __construct(Testimonial $testimonial)
+    {
+        $this->testimonial = $testimonial;
+    }
+}
