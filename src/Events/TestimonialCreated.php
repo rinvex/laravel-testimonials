04a0fc404a6f045630b8618593ad5c9498a314fb
Abdelrahman Omran <me@omranic.com>
2019-12-16 00:47:27 +0400

    Create event classes and map them in the model

--- src/Events/TestimonialCreated.php
+++ src/Events/TestimonialCreated.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rinvex\Testimonials\Events;
 
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Rinvex\Testimonials\Models\Testimonial;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
@@ -22,5 +23,35 @@ class TestimonialCreated implements ShouldBroadcast
     public function __construct(Testimonial $testimonial)
     {
         $this->testimonial = $testimonial;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel
+     */
+    public function broadcastOn()
+    {
+        return new Channel($this->formatChannelName());
+    }
+
+    /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastAs()
+    {
+        return 'rinvex.testimonials.created';
+    }
+
+    /**
+     * Format channel name.
+     *
+     * @return string
+     */
+    protected function formatChannelName(): string
+    {
+        return 'rinvex.testimonials.count';
     }
 }
